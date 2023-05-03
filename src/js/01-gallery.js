e08e62e5a52864b85839bccf13b357d3d2d04b6e
stilistica <119519958+stilistica@users.noreply.github.com>
2023-05-03 16:11:32 +0300

    task_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const ul = document.querySelector(".gallery");
+const imgMakup = createGalleryMarkup(galleryItems);
+
+ul.insertAdjacentHTML('beforeend', imgMakup);
+
+function createGalleryMarkup(gallery) {
+	return gallery.map(({ preview, original, description }) => {
+		return `
+		<li class="gallery__item">
+		<a class="gallery__link" href="${original}">
+		<img src="${preview}" alt="${description}" data-source="${original}" class="gallery__image" />
+		</a>
+		</li>
+		`
+	}).join("");
+} 
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
 
 console.log(galleryItems);
